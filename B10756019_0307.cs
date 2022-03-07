78daf5a28b142c9837cb861828e43f7854cd55a2
a31965118 <50163552+a31965118@users.noreply.github.com>
2022-03-07 11:55:29 +0800

    Add files via upload

---
+++ B10756019_0307.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string x;
+            //int JJ = 555;
+            //x = "B211";
+            //Console.WriteLine(x);
+            //int JJ =Console.ReadLine(JJ);
+
+            double cost = 100;
+            int tip = 15;
+            int tax = 8;
+            Console.Write("你的餐費總金額:");
+            Console.WriteLine(cost + tip + tax); //加總
+        }
+    }
+}
